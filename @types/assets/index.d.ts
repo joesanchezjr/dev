645eb37f319a9a6894b4818ce2b95af4454c96ce
Joe Sanchez Jr <joe@joesanchezjr.com>
2021-06-08 15:39:21 -0500

    redeclared module

--- @types/assets/index.d.ts
+++ @types/assets/index.d.ts
@@ -1,6 +1,1 @@
-declare module "\*.svg" {
-  import React = require("react");
-  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
-}
+declare module "*.svg"
