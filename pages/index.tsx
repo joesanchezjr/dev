20e0aaedb21085a4659f25b018c214a3af080bd6
Joe Sanchez Jr <joe@joesanchezjr.com>
2021-06-09 21:32:40 -0500

    working marquee

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,27 @@ const IndexPage = () => (
         with a team of good people making good things.
       </p>
     </section>
-    <section className="marquee py-4 relative overflow-hidden w-full">
-      <ul className="marquee-list uppercase font-bold text-xl flex space-x-8 list-disc list-outside transform translate-x-6 pl-6">
+    <section className="marquee-container overflow-hidden flex relative w-full p-4">
+      <ul className="marquee uppercase font-bold text-xl flex space-x-8 list-disc list-outside pl-8">
+        <li>React</li>
+        <li>Next</li>
+        <li>Gatsby</li>
+        <li>Create React App</li>
+        <li>WordPress</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>SCSS</li>
+        <li>CSS-in-JS</li>
+        <li>Styled Components</li>
+        <li>Bootstrap</li>
+        <li>Tailwind CSS</li>
+        <li>JavaScript</li>
+        <li>Git</li>
+        <li>GraphQL</li>
+        <li>Node</li>
+        <li>VS Code</li>
+      </ul>
+      <ul className="marquee uppercase font-bold text-xl flex space-x-8 list-disc list-outside pl-8">
         <li>React</li>
         <li>Next</li>
         <li>Gatsby</li>
