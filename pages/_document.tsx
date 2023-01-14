403af5dca97ce6b33e7aa69138f840636467c10f
Joe Sanchez Jr <joe@joesanchezjr.com>
2023-01-14 16:13:12 -0600

    added back cf beacon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document"
 
-// #141539
-
 export default function Document() {
   return (
     <Html lang="en" className="bg-slate-50 text-slate-50">
@@ -9,6 +7,11 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "f95f7928ffdf42758a3fcc9bede5d584"}'
+        ></script>
       </body>
     </Html>
   )
