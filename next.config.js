b78d68f87ba98c913b6efc08c43a5cb2b080b4f4
Joe Sanchez Jr <joe@joesanchezjr.com>
2023-01-14 16:14:19 -0600

    2.0.0 (#1)
    * Moved over code from demo repo
    
    * added back cf beacon

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    })
-    return config
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,27 +1,18 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document"
+import { Html, Head, Main, NextScript } from "next/document"
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-          <script
-            defer
-            src="https://static.cloudflareinsights.com/beacon.min.js"
-            data-cf-beacon='{"token": "f95f7928ffdf42758a3fcc9bede5d584"}'
-          ></script>
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html lang="en" className="bg-slate-50 text-slate-50">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "f95f7928ffdf42758a3fcc9bede5d584"}'
+        ></script>
+      </body>
+    </Html>
+  )
 }
-
-export default MyDocument
