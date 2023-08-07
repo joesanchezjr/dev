819854c24099c2e1e7e6afdc456074f1cdf2c93f
Joe Sanchez Jr <joe@joesanchezjr.com>
2023-08-06 21:45:56 -0500

    syntax highlighting

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,5 +1,8 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
-import highlight from "rehype-highlight";
+import remarkGfm from "remark-gfm";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 /** @type {import('contentlayer/source-files').ComputedFields} */
 const computedFields = {
@@ -82,11 +85,37 @@ export const Experience = defineDocumentType(() => ({
 export default makeSource({
   contentDirPath: "./src/_content",
   documentTypes: [Post, Page, Experience],
-  markdown: { rehypePlugins: [highlight] },
-  mdx: { rehypePlugins: [highlight] },
-  fieldOptions: {
-    date: {
-      timezone: "America/Chicago",
-    },
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: "github-dark",
+          onVisitLine(node) {
+            // Prevent lines from collapsing in `display: grid` mode, and allow empty
+            // lines to be copy/pasted
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }];
+            }
+          },
+          onVisitHighlightedLine(node) {
+            node.properties.className.push("line--highlighted");
+          },
+          onVisitHighlightedWord(node) {
+            node.properties.className = ["word--highlighted"];
+          },
+        },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["anchor"],
+          },
+        },
+      ],
+    ],
   },
 });
