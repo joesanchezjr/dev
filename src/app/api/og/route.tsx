f9882a261c5efb943cb74e6ad9ee15ab3c349f18
Joe Sanchez Jr <joe@joesanchezjr.com>
2023-08-07 20:15:54 -0500

    edge config

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,8 +1,11 @@
 import { ImageResponse } from "next/server";
+import { get } from "@vercel/edge-config";
 
 export const runtime = "edge";
 
 export async function GET() {
+  const available = await get("availableForHire");
+
   const interMedium = await fetch(
     new URL("../../../../public/fonts/inter/Inter-Medium.otf", import.meta.url)
   ).then((res) => res.arrayBuffer());
@@ -13,23 +16,25 @@ export async function GET() {
         style={{ fontFamily: "Inter" }}
       >
         <div tw="w-full h-full flex flex-col justify-between rounded-lg border-4 border-slate-300 p-8">
-          <div tw="flex justify-end">
-            <div tw="flex items-center rounded-full bg-green-200/50 px-6 py-3 ">
-              <span tw="relative flex h-6 w-6 mr-4">
-                <span tw="relative flex h-6 w-6 rounded-full bg-green-500" />
-              </span>
-              <span tw="text-6xl font-medium">Available for hire</span>
+          {available && (
+            <div tw="flex justify-end">
+              <div tw="flex items-center rounded-full bg-green-200/50 px-6 py-3 ">
+                <span tw="relative flex h-6 w-6 mr-4">
+                  <span tw="relative flex h-6 w-6 rounded-full bg-green-500" />
+                </span>
+                <span tw="text-6xl font-medium">Available for hire</span>
+              </div>
             </div>
-          </div>
+          )}
 
-          <div tw="flex flex-col">
+          <div tw={`flex flex-col ${available ? "" : "mt-auto"}`}>
             <div tw="text-7xl font-medium mb-8">Joe Sanchez Jr</div>
             <div tw="relative flex items-end justify-between">
               <div tw="flex flex-col text-6xl font-medium text-slate-900">
                 <div tw="mb-4">Senior Front-End/UI Engineer, Web</div>
                 <div>⚛️ React + TypeScript</div>
               </div>
-              <div tw="absolute bottom-0 right-0 text-5xl font-medium">
+              <div tw={`absolute ${available ? "bottom-0" : "-top-80"} right-0 text-5xl font-medium`}>
                 📍 Austin, Texas
               </div>
             </div>
