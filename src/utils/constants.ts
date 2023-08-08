50a740daf51b1036aab3777f4afa992c72e040d4
Joe Sanchez Jr <joe@joesanchezjr.com>
2023-08-07 20:34:41 -0500

    Setting base url in prod

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,9 +2,13 @@ export const IS_DEV =
   process.env.VERCEL_ENV === "development" ||
   process.env.NODE_ENV === "development";
 
+export const IS_PROD = process.env.VERCEL_ENV === "production";
+
 export const DATABASE = IS_DEV ? "myFirstDatabase" : "authDb";
 
-export const BASE_URL = IS_DEV
+export const BASE_URL = IS_PROD
+  ? "https://www.joesanchezjr.dev"
+  : IS_DEV
   ? "http://localhost:3000"
   : `https://${process.env.VERCEL_URL}` || "https://joesanchezjr.dev";
 
