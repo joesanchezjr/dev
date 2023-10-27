111414436627461fe72db24e90cc69d9b2c0cd03
Joe Sanchez Jr <joe@joesanchezjr.com>
2023-10-27 17:41:02 -0500

    Added skills, updated header (#44)

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,25 +1,31 @@
 import clsx from "clsx";
+import React from "react";
 
 export function CardSection({
   children,
   className,
   variant = "solid",
+  as: Component = "section",
+  noPadding,
   ...props
 }: {
   children: React.ReactNode;
   className?: string;
   variant?: "solid" | "outline";
+  as?: React.ElementType;
+  noPadding?: boolean;
 }) {
-  const innerClasses = clsx("mx-auto max-w-screen-xl px-4", className);
+  const innerClasses = clsx(noPadding ? "max-width-no-padding" : "max-width", className);
   const outerClasses = clsx(
-    "rounded-md p-4 shadow-sm",
+    "rounded-md shadow-sm p-4",
     variant === "solid"
       ? "bg-slate-100 dark:bg-slate-900"
       : "border border-slate-200 dark:border-slate-800"
   );
+
   return (
-    <section className={innerClasses} {...props}>
+    <Component className={innerClasses} {...props}>
       <div className={outerClasses}>{children}</div>
-    </section>
+    </Component>
   );
 }
