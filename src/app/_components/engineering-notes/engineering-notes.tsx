6fba98a92cd2b2a2ec1a61f1bfd1568023dbc84e
Joe Sanchez Jr <joe@joesanchezjr.com>
2023-12-06 17:05:34 -0600

    updating post link

--- src/app/_components/engineering-notes/engineering-notes.tsx
+++ src/app/_components/engineering-notes/engineering-notes.tsx
@@ -1,11 +1,51 @@
 import { Card } from "@/components/card/card";
 import { formatDate } from "@/utils/date";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import { allPosts } from "contentlayer/generated";
 import { compareDesc } from "date-fns";
 import Link from "next/link";
 import title from "title";
+
+function PostLinkListItem({
+  post,
+  isLast,
+}: {
+  post: (typeof allPosts)[0];
+  isLast?: boolean;
+}) {
+  return (
+    <li className="relative flex">
+      <div
+        className={clsx(
+          "absolute left-0 top-0 flex w-4 justify-center",
+          isLast ? "h-6" : "-bottom-6",
+        )}
+      >
+        <div className="w-px bg-slate-200 dark:bg-slate-800" />
+      </div>
+      <div className="absolute flex h-6 w-4 flex-none items-center justify-center bg-slate-50 dark:bg-slate-950">
+        <div className="h-1.5 w-1.5 rounded-full bg-slate-200 ring-1 ring-slate-300 dark:bg-slate-800 dark:ring-slate-700" />
+      </div>
+      <Link
+        href={post.slug}
+        className="flex w-full flex-col md:flex-row md:justify-between md:gap-2 ml-6"
+      >
+        <time
+          dateTime={post.date}
+          className="flex-none py-0.5 text-xs leading-5 tracking-tight text-slate-500 md:order-2"
+        >
+          {formatDate(post.date, "MMM dd")}
+        </time>
+        <span className="max-w-[85ch] flex-auto space-x-2 py-0.5 leading-5">
+          <span className="font-medium text-black dark:text-white">
+            {post.overrideTitleCasing ? post.title : title(post.title)}
+          </span>
+          <span>{post.description}</span>
+        </span>
+      </Link>
+    </li>
+  );
+}
 
 export function EngineeringNotes({ showAll }: { showAll?: boolean }) {
   const sortedPosts = allPosts.sort((a, z) =>
@@ -25,36 +65,11 @@ export function EngineeringNotes({ showAll }: { showAll?: boolean }) {
     >
       <ul role="list" className="-ml-1 space-y-6">
         {postsToShow.map((post, index) => (
-          <li key={post.title}>
-            <Link href={post.slug} className="relative flex gap-x-1">
-              <div
-                className={clsx(
-                  index === postsToShow.length - 1 ? "h-6" : "-bottom-6",
-                  "absolute left-0 top-0 flex w-4 justify-center",
-                )}
-              >
-                <div className="w-px bg-slate-200 dark:bg-slate-800" />
-              </div>
-
-              <div className="relative flex h-6 w-4 flex-none items-center justify-center bg-slate-50 dark:bg-slate-950">
-                <div className="h-1.5 w-1.5 rounded-full bg-slate-200 ring-1 ring-slate-300 dark:bg-slate-800 dark:ring-slate-700" />
-              </div>
-              <div className="flex w-full flex-col md:flex-row md:justify-between md:gap-2">
-                <time
-                  dateTime={post.date}
-                  className="flex-none py-0.5 text-xs leading-5 tracking-tight text-slate-500 md:order-2"
-                >
-                  {formatDate(post.date, "MMM dd")}
-                </time>
-                <p className="max-w-[85ch] flex-auto space-x-2 py-0.5 leading-5">
-                  <span className="font-medium text-black dark:text-white">
-                    {post.overrideTitleCasing ? post.title : title(post.title)}
-                  </span>
-                  <span>{post.description}</span>
-                </p>
-              </div>
-            </Link>
-          </li>
+          <PostLinkListItem
+            key={post.title}
+            post={post}
+            isLast={index === postsToShow.length - 1}
+          />
         ))}
       </ul>
     </Card>
