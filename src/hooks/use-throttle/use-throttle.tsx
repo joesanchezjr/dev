b0ae4918f99adecfdd13d7bcb1a5e57800790c8e
Joe Sanchez Jr <joe@joesanchezjr.com>
2024-03-03 11:57:03 -0600

    added useThrottle hook

---
+++ src/hooks/use-throttle/use-throttle.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function useThrottle<T>(value: T, interval = 300) {
+  const now = Date.now();
+  const [throttledValue, setThrottledValue] = React.useState(value);
+  const lastUpdated = React.useRef(now);
+
+  React.useEffect(() => {
+    if (now >= lastUpdated.current + interval) {
+      lastUpdated.current = now;
+      setThrottledValue(value);
+    } else {
+      const timeout = setTimeout(() => {
+        lastUpdated.current = now;
+        setThrottledValue(value);
+      }, interval);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [value, interval]);
+
+
+  return throttledValue;
+}
