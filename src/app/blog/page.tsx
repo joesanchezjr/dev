bddd93727fa15c2e0a5a14f1d76d8bb706295b1f
Joe Sanchez Jr <joe@joesanchezjr.com>
2024-03-25 19:15:54 -0500

    updated post list styles

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,23 +8,29 @@ export default async function BlogPage() {
   const allBlogs = await getBlogPosts()
 
   return (
-    <div className="space-y-4">
+    <div>
       {allBlogs.map((post) => (
-        <Link key={post.slug} className="group flex flex-col" href={`/blog/${post.slug}`}>
-          <div className="flex w-full flex-col border-l border-l-zinc-300 pl-4 transition-colors group-hover:border-l-indigo-500 dark:border-l-zinc-700">
-            <h2 className="text-lg font-medium tracking-tight text-neutral-900 dark:text-neutral-100">
-              {post.metadata.title}
-            </h2>
-            {typeof post.metadata.excerpt === "string" && (
-              <div className="prose prose-p:mb-2 text-sm text-zinc-500">
-                <MdxRenderer source={post.metadata.excerpt} />
-                <div>
-                  <time className="text-sm text-zinc-500">
-                    {formatDate(post.metadata.publishedAt as string, "MMMM dd, yyyy")}
-                  </time>
+        <Link
+          key={post.slug}
+          className="group flex flex-col first-of-type:pt-0 last-of-type:pb-0 "
+          href={`/blog/${post.slug}`}
+        >
+          <div className="flex w-full flex-col py-4 transition-colors hover:bg-zinc-200 dark:hover:bg-zinc-800">
+            <div className="border-l border-l-zinc-300 pl-4 group-hover:border-l-indigo-500 dark:border-l-zinc-700 ">
+              <h2 className="text-lg font-medium tracking-tight text-neutral-900 dark:text-neutral-100">
+                {post.metadata.title}
+              </h2>
+              {typeof post.metadata.excerpt === "string" && (
+                <div className="prose prose-p:mb-2 text-sm text-zinc-500">
+                  <MdxRenderer source={post.metadata.excerpt} />
+                  <div>
+                    <time className="text-sm text-zinc-500">
+                      {formatDate(post.metadata.publishedAt as string, "MMMM dd, yyyy")}
+                    </time>
+                  </div>
                 </div>
-              </div>
-            )}
+              )}
+            </div>
           </div>
         </Link>
       ))}
--- src/components/header/page-title.tsx
+++ src/components/header/page-title.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { cn } from "@/lib/utils"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
@@ -19,12 +20,20 @@ export function PageTitle() {
   return (
     <div className="flex items-center">
       <Link href="/" className="mr-2">
-        <h1 className="text-lg font-medium">Joe Sanchez Jr</h1>
+        <h1
+          className={cn(
+            "text-lg font-medium transition-all",
+            pathname !== "/" &&
+              "underline decoration-zinc-300 underline-offset-4  hover:decoration-zinc-700 dark:decoration-zinc-600 dark:hover:decoration-zinc-300",
+          )}
+        >
+          Joe Sanchez Jr
+        </h1>
       </Link>
       {title === null ? null : (
         <>
-          <span className="text-zinc-500">/</span>
-          <h2 className="text-lg font-medium ml-2">{title}</h2>
+          <span className="text-zinc-300">/</span>
+          <h2 className="ml-2 text-lg font-medium ">{title}</h2>
         </>
       )}
     </div>
