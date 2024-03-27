d1e5c957a0b759bb0c335fffe1a433e2faa7f092
Joe Sanchez Jr <joe@joesanchezjr.com>
2024-03-26 21:15:56 -0500

    removed middleware check

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,15 +3,10 @@ import type { NextRequest } from "next/server"
 import { nanoid } from "nanoid"
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === "/blog") {
-    return NextResponse.redirect(new URL(`/`, request.url))
-  }
-  if (request.nextUrl.pathname === "/resume") {
-    const id = nanoid()
-    return NextResponse.redirect(new URL(`/documents/resume.pdf?v=${id}`, request.url))
-  }
+  const id = nanoid()
+  return NextResponse.redirect(new URL(`/documents/resume.pdf?v=${id}`, request.url))
 }
 
 export const config = {
-  matcher: ["/resume", "/blog"],
+  matcher: ["/resume"],
 }
