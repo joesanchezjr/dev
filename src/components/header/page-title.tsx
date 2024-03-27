e0c451c8c396c28dc6137cf78cb84ad0e6994914
Joe Sanchez Jr <joe@joesanchezjr.com>
2024-03-26 20:34:13 -0500

    added more links

--- src/components/header/page-title.tsx
+++ src/components/header/page-title.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { cn } from "@/lib/utils"
+import { CornerRightDown } from "lucide-react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
@@ -17,6 +18,9 @@ export function PageTitle() {
     default:
       title = null
   }
+
+  const blogSlug = pathname.includes("/blog/") && pathname.split("/").length === 3
+
   return (
     <div className="flex items-center">
       <Link href="/" className="mr-2">
@@ -33,7 +37,23 @@ export function PageTitle() {
       {title === null ? null : (
         <>
           <span className="text-zinc-300">/</span>
-          <h2 className="ml-2 text-lg font-medium ">{title}</h2>
+          {blogSlug ? (
+            <Link href="/blog">
+              <h2 className="mx-2 font-medium underline decoration-zinc-300 underline-offset-4 hover:decoration-zinc-700 dark:decoration-zinc-600 dark:hover:decoration-zinc-300">
+                {title}
+              </h2>
+            </Link>
+          ) : (
+            <h2 className="mx-2 font-medium">{title}</h2>
+          )}
+        </>
+      )}
+      {!blogSlug ? null : (
+        <>
+          <span className="text-zinc-300">/</span>
+          <div className="ml-2 flex items-center text-sm font-medium ">
+            <CornerRightDown className="h-4 w-4" />
+          </div>
         </>
       )}
     </div>
