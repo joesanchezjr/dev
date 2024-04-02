9cb7157555b15778cc4b7079efd41a807df5d600
Joe Sanchez Jr <joe@joesanchezjr.com>
2024-04-02 14:38:16 -0500

    one fn

--- src/utils/mdx.ts
+++ src/utils/mdx.ts
@@ -16,67 +16,44 @@ export type MdxMetadata = {
 const POSTS_DIRECTORY = path.join(process.cwd(), "src", "content")
 
 /**
- * Read and parse and MDX file
- */
-async function readAndParseMdxFile(filePath: string) {
-  try {
-    const rawContent = await readFile(filePath, "utf-8") // Read the file
-    return matter(rawContent, { excerpt: true }) // Return the parsed frontmatter and content
-  } catch (error) {
-    notFound()
-  }
-}
-
-/**
- * Get all MDX files in a directory
- */
-async function getAllMdxFilesInDirectory() {
-  try {
-    const files = await readdir(POSTS_DIRECTORY) // Read all files in the directory
-    const mdxFiles = files.filter((file) => path.extname(file) === ".mdx") // Filter out only the mdx files
-    return mdxFiles // Return the list of mdx files
-  } catch (error) {
-    console.error(error)
-    throw new Error("Error reading directory")
-  }
-}
-
-/**
- * Get MDX data for all files in a directory
- */
-async function getAllMdxData() {
-  try {
-    const mdxFiles = await getAllMdxFilesInDirectory() // Get all mdx files in the directory
-    const mdxData = await Promise.allSettled(
-      mdxFiles.map(async (file) => {
-        const { data, content, excerpt } = await readAndParseMdxFile(path.join(POSTS_DIRECTORY, file)) // Read the file
-        const slug = path.basename(file, path.extname(file)) // Get the slug
-        return { metadata: data, slug, content, excerpt } // Return the metadata, slug, content, and excerpt
-      }),
-    )
-    return mdxData
-      .map((promise) => {
-        if (promise.status === "fulfilled") {
-          return promise.value
-        }
-      })
-      .filter(isTruthy) // Return the MDX data for fulfilled promises
-  } catch (error) {
-    console.error(error)
-    throw new Error("Error getting MDX data")
-  }
-}
-
-/**
  * Get all blog posts
  * using cache so that we can use the data without making multiple requests
  */
 export const getAllBlogPosts = cache(async () => {
-  const posts = await getAllMdxData()
-  const sortedPosts = posts.sort((a, z) => {
-    return new Date(z.metadata.publishedAt as string).getTime() - new Date(a.metadata.publishedAt as string).getTime()
-  })
-  return sortedPosts
+  let markdownFiles
+
+  try {
+    markdownFiles = await readdir(POSTS_DIRECTORY) // Read all files in the directory
+  } catch (error) {
+    console.error(error)
+    throw new Error("Error reading directory")
+  }
+
+  try {
+    const posts = markdownFiles.filter((file) => [".mdx", ".md"].includes(path.extname(file))) // Filter out only the md/mdx files
+    const postsWithMarkdownData = await Promise.all(
+      posts.map(async (file) => {
+        let postContent
+        try {
+          postContent = await readFile(path.join(POSTS_DIRECTORY, file), "utf-8") // Read the file
+        } catch (error) {
+          notFound()
+        }
+        const { data, content, excerpt } = matter(postContent, { excerpt: true }) // Return the parsed frontmatter and content
+        const slug = path.basename(file, path.extname(file)) // Get the slug
+        return { metadata: data, slug, content, excerpt } // Return the metadata, slug, content, and excerpt
+      }),
+    )
+
+    const filteredAndSortedPosts = postsWithMarkdownData.filter(isTruthy).sort((a, z) => {
+      return new Date(z.metadata.publishedAt as string).getTime() - new Date(a.metadata.publishedAt as string).getTime()
+    })
+
+    return filteredAndSortedPosts
+  } catch (error) {
+    console.error(error)
+    notFound()
+  }
 })
 
 /**
